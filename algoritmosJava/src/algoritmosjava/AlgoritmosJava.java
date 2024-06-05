7b4c4245037179af7f7a20a02efaa5cc3d128ddb
pnzrfst <jxssxp@gmail.com>
2024-06-04 21:20:54 -0300

    resolvendo exercicios propostos pelo Gustavo

--- algoritmosJava/src/algoritmosjava/AlgoritmosJava.java
+++ algoritmosJava/src/algoritmosjava/AlgoritmosJava.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class AlgoritmosJava {
     
     public static void main(String[] args) {
-        /* // ler um número e escrever seu antecessor //
+        // ler um número e escrever seu antecessor //
         
         //Scanner leia = new Scanner(System.in);
         
@@ -25,6 +25,9 @@ public class AlgoritmosJava {
         
         System.out.println("o antecessor do seu número é: " + antecessorCitado + " e o sucessor é: " + sucessorCitado);*/
         
+        
+
+
         // area total de um retangulo //
         
         /*Scanner leia = new Scanner(System.in);
@@ -42,31 +45,58 @@ public class AlgoritmosJava {
         
         System.out.println("A area total do seu retangulo é de:" + areaTotal);*/
         
-        Scanner leia = new Scanner(System.in);
+        
+        // total da idade em dias //
+        
+        /*Scanner leia = new Scanner(System.in);
         LocalDate dataAtual = LocalDate.now();
         
-        System.out.println("Escreva seu mes de nascimento: ");
-        String mesNascimento = leia.nextLine();
+        System.out.println("Escreva sua data de nascimento (no formato anos-meses-dias): ");
+        String dataNasc = leia.nextLine();
         
-        System.out.println("Escreva o ano do seu nascimento: ");
-        String anoNascimento = leia.nextLine(); 
+        LocalDate dataNascimento = LocalDate.parse(dataNasc);
         
-        System.out.println("Escreva o dia do seu nascimento: ");
-        String diaNascimento = leia.nextLine();
+        long totalIdade = dataAtual.toEpochDay()- dataNascimento.toEpochDay();
+     
+        System.out.println("A sua idade em dias é de: " + totalIdade + " dias");*/
         
-        long dataConvert = dataAtual.toEpochDay(); 
+        // total de eleitores de um municipio //
         
-        int diaNasc = Integer.parseInt(diaNascimento);
-        int mesNasc = Integer.parseInt(mesNascimento);
-        int anoNasc = Integer.parseInt(anoNascimento);
+        /*Scanner leia = new Scanner(System.in);
+        
+        System.out.println("Escreva o total de eleitores do seu municipio? ");
+        double totalEleitores = leia.nextDouble();
+        
+        System.out.println("Qual foi o total de votos brancos? ");
+        double votosBrancos = leia.nextDouble();
+        
+        System.out.println("Qual foi o total de votos nulos?");
+        double votosNulos = leia.nextDouble(); 
+        
+        System.out.println("Qual foi o total de votos válidos? ");
+        double totalValidos  = leia.nextDouble();
+       
+        double percentualBrancos = ((votosBrancos/totalEleitores)*100);
+        double percentualNulos = ((votosNulos/totalEleitores)*100);
+        double percentualValidos = ((totalValidos/totalEleitores)*100);
+        
+        System.out.println("O total de eleitores foi" + totalEleitores + " sendo: " + percentualBrancos + " brancos" + percentualNulos + " nulos e" + percentualValidos + " validos");*/
+        
+        Scanner leia = new Scanner(System.in);
+        
+        System.out.println("Qual a temperatura? (em C): ");
+        double tempCelsius = leia.nextDouble();
+        
+        double tempFahrenheit = (tempCelsius * 1.8) + 32;
+        double tempKelvin = tempCelsius + 273.15;
+        
+        System.out.println("A sua temperatura em celsius é de: " + tempCelsius +"C" + ", em Fahrenheit é de: " + tempFahrenheit + "F" + " e em Kelvin é de: " + tempKelvin + "K");
         
         
-        
-        long totalAnos = (((mesNasc * 30) + (anoNasc * 365) + diaNasc));
-        long totalDias = 
-        System.out.println("a sua idade em dias é de: " + totalAnos);
         
         
     }
+    
+    
     
 }
