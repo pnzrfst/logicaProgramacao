35459ca182574f16bac63212ed57f68c1f53274f
pnzrfst <jxssxp@gmail.com>
2024-07-01 16:04:27 -0300

    subindo os exercicios

--- 03-estruturasRepeticao/src/exercicios.js
+++ 03-estruturasRepeticao/src/exercicios.js
@@ -125,15 +125,13 @@ do{
      
 }while(numero !== 0)*/
 
-
 /*Valentina tem 1.50m e cresce 2 centímetros por ano, enquanto Joãozinho, seu irmão mais
 novo, tem 1.40m e cresce 3 centímetros por ano. Crie um algoritmo que mostre quando
-(quantos anos) Enzo será mais alto que Valentina.*/
+(quantos anos) Enzo será mais alto que Valentina.
 
 var alturaVal = 1.50
 var alturaJoao = 1.40
 var anos = 0 
-
 
 while(alturaVal >= alturaJoao){
     alturaVal += 0.02;
@@ -143,4 +141,173 @@ while(alturaVal >= alturaJoao){
 
 console.log(`A altura da Valentina ${alturaVal.toFixed(2)}`);
 console.log(`A altura do Joao ${alturaJoao.toFixed(2)}`);
-console.log(`Vão demorar ${anos} anos para Joao ser mais alto Valentina.`);
+console.log(`Vão demorar ${anos} anos para Joao ser mais alto Valentina.`);*/
+
+/*Foi feita uma pesquisa entre os habitantes de um a região e coletados os dados de altura e
+gênero (0=masc , 1=fem) das pessoas. Faça um programa que leia os dados de 10 pessoas e
+informe:
+– a maior e a menor altura encontrada;
+– a média de altura das mulheres;
+– a média de altura da população;
+– o percentual de homens na população.
+
+
+var somaAlturaMulheres = 0;
+var totalHomens = 0;     
+var totalMulheres = 0;
+var totalHomens = 0;
+var totalAlturaPopulacao = 0;
+let menorAltura = Infinity;
+let maiorAltura = -Infinity;
+
+for(var contador = 0; contador <=10; contador++){
+ var alturaHab = leia.questionFloat("digite sua altura: ");
+
+ if(alturaHab < menorAltura){
+    menorAltura = alturaHab;
+ }
+
+ if(alturaHab > maiorAltura){
+    maiorAltura = alturaHab;
+ }
+
+ totalAlturaPopulacao += alturaHab;
+
+ var generoHab = leia.questionInt("digite seu genero: (0) masculino   (1)feminino ");
+
+    if(generoHab === 0){
+        totalHomens++
+    }else if(generoHab === 1){
+        totalMulheres++
+        somaAlturaMulheres += totalMulheres;
+    }else{
+        console.log("valor invalido, por favor, preencha um numero valido.");
+        contador--;
+    }
+}
+
+var mediaMulheres = somaAlturaMulheres / totalMulheres;
+
+var mediaPopulacao = totalAlturaPopulacao / 10;
+
+var percentualHomens = (totalHomens / 10) * 100;
+
+console.log(`menor altura: ${menorAltura}, maior altura: ${maiorAltura}`);
+console.log(`media de altura das mulheres: ${mediaMulheres.toFixed(2)}`);
+console.log(`media de altura da populacao: ${mediaPopulacao.toFixed(2)}`);
+console.log(`percentual de homens na populacao: ${percentualHomens}`);*/
+
+/*Vamos desenvolver um jogo pra gente se divertir um pouco com a criançada, trata-se do
+“Número mágico”. Neste jogo um número deve ser cadastrado pelo ‘mestre’ (PvP) ou sorteado
+pelo game (PvE) e o desafiante deve tentar adivinhar. A cada tentativa incorreta o sistema
+informa se o chute foi mais alto ou mais baixo que o número mágico. Quando o desafiante
+acertar o game informar em quantas tentativas ele conseguiu. No início do jogo o jogador
+escolhe entre PvP e PvE.
+- Sistema de dificuldade para o PvE, onde o jogador escolhe a dificultade e ela
+influencia o tamanho do range possível para o número mágico
+
+var numeroMagico;
+var modoJogo = leia.keyInSelect(["PvP", "PvE"]);
+
+if(modoJogo === 0 ){ 
+    var numeroMagico = leia.questionInt("qual o numero magico: ", {hideEchoBack: true});
+    let tentativas = 0;
+    let palpite;
+
+    do {
+        palpite = leia.questionInt('Desafiante, digite seu palpite: ');
+
+        if (palpite < numeroMagico) {
+            console.log('O número é maior.');
+        } else if (palpite > numeroMagico) {
+            console.log('O número é menor.');
+        }
+
+        tentativas++;
+    } while (palpite !== numeroMagico);
+
+    console.log(`Desafiante acertou o número ${numeroMagico} em ${tentativas} tentativas.`);
+
+}else if (modoJogo === 1){
+    var dificuldadeSelecionada = leia.questionInt("(0) facil; (1) media; (2)dificil ");
+    switch(dificuldadeSelecionada){
+        case 0 : 
+        numeroMagico = parseInt(Math.random() * 10);
+        break;
+        case 1 : 
+        numeroMagico = parseInt(Math.random() * 100);
+        break;
+        case 2 :
+        numeroMagico = parseInt(Math.random() * 1000);
+    }
+    
+    var tentativas = 0
+
+    do{
+        var palpite = leia.questionInt("digite seu palpite: ");
+        if(palpite < numeroMagico){
+            console.log("o numero é maior.");  
+        }
+
+        if(palpite > numeroMagico){
+            console.log("o numero é menor.");
+        }
+
+        if(palpite === numeroMagico){
+            console.log("voce acertou!");
+        }
+        
+        tentativas++
+
+    }while(palpite !== numeroMagico);
+
+    console.log(`voce precisou de ${tentativas} para acertar o numero.`)
+    
+}*/
+
+/* Crie um algoritmo que receba um número e que ele mostre os números da sequência
+fibonacci. (Exemplo, se o usuário digitar 5, deve ser mostrado até o 5 número da sequência
+fibonacci). Lembrando que a sequência fibonacci, sempre é a soma dos dois números
+anteriores, começando sempre em 1 e 1; 1+1 = 2, 2+1 = 3, 3+2 =5.
+
+var numero = leia.questionInt("DIGITE ATE QUAL VALOR QUER: ");
+var anterior = 0, atual = 1, proximo, resultado = "0 - 1";
+
+for(var i = 2; i < numero; i++){
+    proximo = anterior + atual;
+    anterior = atual;
+    atual = proximo;
+    resultado = resultado + " - " + proximo;
+}
+console.log(resultado);*/
+
+
+/*Faça um algoritmo que receba um número e ele calcule o fatorial desse número. O Fatorial, é
+o número vezes todos os valores multiplicados até 1.
+
+var teste = leia.questionNewPassword("DIGITE NUMERO");
+console.log(teste)
+var numero = leia.questionInt("DIGITE UM NUMERO: ")
+var resultado = 1;
+
+while(numero > 0){
+    resultado = resultado * numero;
+    numero--;
+}
+
+console.log("FATORIAL E: " + resultado);*/
+
+/*Crie um algoritmo para realizar a conversão de um número inteiro decimal, para binário. Vale
+lembrar, que para realizar esse exercícios, vocês devem usar a técnica da divisão.*/
+
+var decimal = leia.questionInt("DIGITE UM NUMERO");
+var binario = '';
+
+while(decimal >= 1){
+    var result = parseInt(decimal / 2);
+    var resto = decimal % 2;
+    decimal = result;
+    binario = resto + binario;
+}
+
+console.log(binario)
